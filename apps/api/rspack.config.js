889db31befefd76cb28f27a247baa10996cc1dc5
Abdugani Toshmukhamedov <a.toshmukhamedov@icloud.com>
2025-07-30 14:59:18 +0500

    feat(api): Use rspack for building and serving API
    This commit migrates the API build process from webpack to rspack.
    
    The key changes include:
    
    - Updating the project.json file to use rspack executor. - Creating a rspack.config.js file to
    configure rspack. - Updating the nx.json file to use rspack plugin. - Adding rspack and related
    dependencies to package.json. - Renaming webpack.config.js to rspack.config.js

--- apps/api/rspack.config.js
+++ apps/api/rspack.config.js
@@ -1,17 +1,20 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+// @ts-check
+const { NxAppRspackPlugin } = require('@nx/rspack/app-plugin');
 const { join } = require('path');
 
 module.exports = {
   output: {
     path: join(__dirname, '../../dist/apps/api'),
   },
+  entry: {
+    main: './src/main.ts',
+  },
   plugins: [
-    new NxAppWebpackPlugin({
+    new NxAppRspackPlugin({
       target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
       assets: ['./src/assets'],
+      main: './src/main.ts',
       optimization: false,
       outputHashing: 'none',
       generatePackageJson: true,
