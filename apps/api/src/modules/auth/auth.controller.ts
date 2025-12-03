4e380d4826b9973bdf4c57530a62bc51102569a6
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2025-12-03 17:52:32 +0500

    bugfix: auth problem

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -3,7 +3,11 @@ import { ApiTags } from '@nestjs/swagger';
 import type { Response } from 'express';
 import { GoogleOauthGuard } from '~/common/guards/google-oauth.guard';
 import type { AuthenticatedRequest } from '~/shared/types/request-with-user';
-import { CLIENT_URL, COOKIES_ACCESS_TOKEN_KEY } from '~/utils/constants';
+import {
+  CLIENT_URL,
+  COOKIE_DOMAIN,
+  COOKIES_ACCESS_TOKEN_KEY,
+} from '~/utils/constants';
 // biome-ignore lint/style/useImportType: <throws an error if put type>
 import { AuthService } from './auth.service';
 
@@ -37,6 +41,7 @@ export class AuthController {
       httpOnly: true,
       sameSite: 'none',
       secure: true,
+      domain: COOKIE_DOMAIN,
     });
 
     res.redirect(`${CLIENT_URL}/family-trees`);
@@ -49,6 +54,7 @@ export class AuthController {
       httpOnly: true,
       sameSite: 'none',
       secure: true,
+      domain: COOKIE_DOMAIN,
     });
 
     res.status(200).send();
--- apps/api/src/utils/constants.ts
+++ apps/api/src/utils/constants.ts
@@ -3,3 +3,4 @@ export const COOKIES_ACCESS_TOKEN_KEY = 'access_token';
 export const GLOBAL_PREFIX = 'api';
 export const DICEBEAR_URL = 'https://api.dicebear.com';
 export const CLIENT_URL = 'https://famtree.uz';
+export const COOKIE_DOMAIN = '.famtree.uz';
