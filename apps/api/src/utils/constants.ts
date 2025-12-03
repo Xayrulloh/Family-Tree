9162bb5d41a7471d26aa4c64ea05f3969082acf3
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2025-12-03 15:25:49 +0500

    hotfix: client url constant changed

--- apps/api/src/utils/constants.ts
+++ apps/api/src/utils/constants.ts
@@ -2,4 +2,4 @@ export const COOKIES_CONFIG_KEY = 'cookies';
 export const COOKIES_ACCESS_TOKEN_KEY = 'access_token';
 export const GLOBAL_PREFIX = 'api';
 export const DICEBEAR_URL = 'https://api.dicebear.com';
-export const CLIENT_URL = 'http://localhost:4200';
+export const CLIENT_URL = 'https://famtree.uz';
