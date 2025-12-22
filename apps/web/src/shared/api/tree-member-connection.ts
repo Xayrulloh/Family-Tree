a5be0f1c6a18217bc225b251bc29658e07652db7
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2025-12-22 13:01:16 +0500

    hotfix: query/param dto is now working

--- apps/web/src/shared/api/tree-member-connection.ts
+++ apps/web/src/shared/api/tree-member-connection.ts
@@ -1,10 +1,7 @@
 import type {
-  FamilyTreeMemberConnectionCreateRequestType,
   FamilyTreeMemberConnectionGetAllParamType,
   FamilyTreeMemberConnectionGetAllResponseType,
   FamilyTreeMemberConnectionGetByMemberParamType,
-  FamilyTreeMemberConnectionGetParamType,
-  FamilyTreeMemberConnectionUpdateRequestType,
 } from '@family-tree/shared';
 import type { AxiosRequestConfig } from 'axios';
 import { base } from './base';
@@ -16,37 +13,6 @@ export const treeMemberConnection = {
   ) => {
     return base.get<FamilyTreeMemberConnectionGetAllResponseType>(
       `/family-trees/${param.familyTreeId}/members/connections`,
-      config,
-    );
-  },
-  create: (
-    param: FamilyTreeMemberConnectionGetAllParamType,
-    body: FamilyTreeMemberConnectionCreateRequestType,
-    config?: AxiosRequestConfig,
-  ) => {
-    return base.post<FamilyTreeMemberConnectionGetAllResponseType>(
-      `/family-trees/${param.familyTreeId}/members/connections`,
-      body,
-      config,
-    );
-  },
-  update: (
-    param: FamilyTreeMemberConnectionGetParamType,
-    body: FamilyTreeMemberConnectionUpdateRequestType,
-    config?: AxiosRequestConfig,
-  ) => {
-    return base.put(
-      `/family-trees/${param.familyTreeId}/members/connections/${param.id}`,
-      body,
-      config,
-    );
-  },
-  delete: (
-    param: FamilyTreeMemberConnectionGetParamType,
-    config?: AxiosRequestConfig,
-  ) => {
-    return base.delete(
-      `/family-trees/${param.familyTreeId}/members/connections/${param.id}`,
       config,
     );
   },
