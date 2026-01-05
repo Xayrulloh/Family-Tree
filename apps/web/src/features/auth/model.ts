b049b8365a0179e4f60a997632ab06ed24994fd0
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2026-01-05 15:22:44 +0500

    hotfix: broken redirect url fixed

--- apps/web/src/features/auth/model.ts
+++ apps/web/src/features/auth/model.ts
@@ -1,8 +1,22 @@
 import { createEffect } from 'effector';
 
-// Simulate Google login
+// Simulate Google login + redirect to return_url
 const googleLoginFx = createEffect(async () => {
-  window.location.href = `${import.meta.env.VITE_API_URL}/auth/google`;
+  const returnUrl = localStorage.getItem('auth_redirect_url');
+
+  if (returnUrl) {
+    localStorage.removeItem('auth_redirect_url');
+  }
+
+  const backendLoginUrl = new URL(
+    `${import.meta.env.VITE_API_URL}/auth/google`,
+  );
+
+  if (returnUrl) {
+    backendLoginUrl.searchParams.append('return_url', returnUrl);
+  }
+
+  window.location.href = backendLoginUrl.toString();
 
   return window.location.href;
 });
